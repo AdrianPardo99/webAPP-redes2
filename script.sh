fe48f1d46074e7dc340e1c5cfce2388aa3c60008
Adrian González Pardo <gozapaadr@gmail.com>
2020-06-11 03:14:47 -0500

    Modificación de script a una versión más corta y mejorada.

--- script.sh
+++ script.sh
@@ -7,23 +7,17 @@ var=$(ls $1)
 arr=($(echo $var | tr "\ " "\n"))
 strF=""
 for i in "${arr[@]}";do
-  if [[ "$i" != "js" ]];then
-    if [[ "$i" != "css" ]];then
-      if [[ "$i" != "font" ]];then
-        if [[ "$i" != "iconfont" ]];then
-          if [[ "$i" != "img" ]];then
-            if [[ "$i" != "phpGral" ]];then
-              tipo=$(file $1$i)
-              arr2=($(echo $tipo | tr " " "\n"))
-              if [[ "${arr2[2]}" == "directory" ]];then
-                strF+="<li><a href=\"/$i\">$i</a></li>"
-              fi
-            fi
-          fi
-        fi
+  case $i in
+    css|js|font|iconfont|phpGral|img )
+      continue
+      ;;
+    * )
+      typeD=$(file $1$i | grep "directory")
+      if [[ ! -z $typeD  ]];then
+        strF+="<li><a href=\"/$i\">$i</a></li>"
       fi
-    fi
-  fi
+    ;;
+  esac
 done
 strF+="<li><a href=\"/\">Principal</a></li>"
 echo $strF
