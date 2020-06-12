5a0e54d6d6f744c43e9a0ffeb0bf435202c75042
Adrian González Pardo <gozapaadr@gmail.com>
2020-06-12 17:51:11 -0500

    Modificación cartas de presentación, y js que no se ocupa

--- index.php
+++ index.php
@@ -12,7 +12,13 @@
     <?php include 'phpGral/mainConf.php';?>
     <br>
     <br>
+  </div>
+  <div class="row">
+    <div class="col l2"></div>
+    <div class="col l8 m12 s12">
     <?php include 'phpGral/cartas.php';?>
+    </div>
+    <div class="col l2"></div>
   </div>
   <?php include 'phpGral/foot.php';
   /* Para ejecutar ssh via python es necesario hacerlo con sudo*/
--- phpGral/titulo.php
+++ phpGral/titulo.php
@@ -5,11 +5,6 @@
 <script type = "text/javascript" src = "/js/jquery-3.4.1.min.js"></script>
 <script type = "text/javascript" src="/js/materialize.js"></script>
 <link href="/css/icon.css" rel="stylesheet">
-<?php
-    if($var !="index"){
-      echo "<script type = \"text/javascript\" src =\"js/ajaxJS.js\"></script>\n";
-    }
-  ?>
 </head>
 <body>
   <header>
